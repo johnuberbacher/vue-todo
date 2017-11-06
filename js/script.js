1c3fc39d9f03b82756315b62087392239d47eda1
John Uberbacher <johnuberbacher@gmail.com>
2017-11-05 21:45:39 -0500

    Revert "Removes finished items from the DOM"
    This reverts commit faab680643305ec87382e10b6b3a5f23cc1817a2.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@ var app = new Vue({
 	data: {
 		todoText: '',
 		todos: [
-			{text: 'Remind mom about the upcoming event'},
-			{text: 'Buy eggs and milk'},
-			{text: 'Set the clocks back on the appliances'},
+			{text: 'Buy eggs and milk', complete: true},
+			{text: 'Remind mom about the upcoming event', complete: false},
 		]
 	},
 	methods: {
@@ -13,14 +12,9 @@ var app = new Vue({
 			var newTodo = this.todoText.trim();
 			if (!newTodo) {return;}
 			this.todos.push(
-				{text: newTodo}
+				{text: newTodo, complete: false}
 			);
 			this.todoText = '';
-		},
-		// Pass in the current to-do item
-		removeTodo: function(todo) {
-			// remove it from the list using array.splice(start, deleteCount)
-			this.todos.splice(this.todos.indexOf(todo), 1)
 		}
 	}
 });
